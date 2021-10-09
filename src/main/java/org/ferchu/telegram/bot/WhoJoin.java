1c3619ca8300f09ce9f51329a6a2793ba2c9041f
Fernando Luján Martínez <8flm95@gmail.com>
2021-10-09 19:23:49 +0200

    First commit
    First commit

---
+++ src/main/java/org/ferchu/telegram/bot/WhoJoin.java
@@ -0,0 +1,36 @@
+package org.ferchu.telegram.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class WhoJoin extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "Attendance bot, working on it.";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1982309467:AAEmzlI2dQmP1OlaKzroM8UGsZ7QUTPnVQk";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        final String messageTextReceived = update.getMessage().getText();
+
+        // Se obtiene el id de chat del usuario
+        final long chatId = update.getMessage().getChatId();
+
+        // Se crea un objeto mensaje
+        SendMessage message = new SendMessage().setChatId(chatId).setText(messageTextReceived);
+
+        try {
+            // Se envía el mensaje
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
