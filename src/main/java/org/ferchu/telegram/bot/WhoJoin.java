f74b8eb678d35591389a684ea920918fccb0b576
Fernando Luján Martínez <8flm95@gmail.com>
2021-10-12 13:55:38 +0200

    Added poll support

--- src/main/java/org/ferchu/telegram/bot/WhoJoin.java
+++ src/main/java/org/ferchu/telegram/bot/WhoJoin.java
@@ -1,9 +1,14 @@
 package org.ferchu.telegram.bot;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WhoJoin extends TelegramLongPollingBot {
 
@@ -24,16 +29,36 @@ public class WhoJoin extends TelegramLongPollingBot {
         // Se obtiene el id de chat del usuario
         final long chatId = update.getMessage().getChatId();
 
-        // Se crea un objeto mensaje
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText(messageTextReceived + "putazorra");
-        try {
-            // Se envía el mensaje
+        if(messageTextReceived == "poll") {
 
-            execute(message);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            SendPoll poll = new SendPoll();
+            poll.setChatId(String.valueOf(chatId));
+            poll.setQuestion("¿Eres gilipollas?");
+            List<String> options = new ArrayList<>();
+            options.add("Sí");
+            options.add("Sí");
+            poll.setOptions(options);
+
+            try {
+                // Se envía el mensaje
+
+                execute(poll);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            // Se crea un objeto mensaje
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+            message.setText(messageTextReceived + "putazorra");
+            try {
+                // Se envía el mensaje
+
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
     }
 
---
+++ src/main/java/org/ferchu/telegram/bot/commands/HelpCommand.java
@@ -0,0 +1,6 @@
+package org.ferchu.telegram.bot.commands;
+
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+
+public class HelpCommand extends BotCommand {
+}
